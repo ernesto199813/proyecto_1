bd57d10c6311e5137a6f11d2cddcaf5d56d497d5
ernesto <ernesto199813@gmail.com>
2022-05-30 16:42:18 -0400

    pagina_1

---
+++ pagina.py
@@ -0,0 +1,25 @@
+import re 
+
+import sqlite3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+
+    return render_template ('home.html')
+
+
+@app.route('/inicio')
+
+def inicio():
+
+    return render_template ('inicio.html')
+
+
+if __name__  ==  '__main__' : 
+     app.run(debug=True)
+
